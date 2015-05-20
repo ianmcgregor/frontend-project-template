b672b854f92d2129062569719b3615868fec5598
ianmcgregor <imcgrgr@gmail.com>
2015-05-21 00:25:48 +0100

    tidies up test setup

--- karma.conf.js
+++ karma.conf.js
@@ -1,89 +1,82 @@
-// Karma configuration
+'use strict';
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-    plugins: [
-      'karma-mocha',
-      'karma-chai',
-      'karma-browserify',
-      'karma-chrome-launcher',
-      'karma-firefox-launcher'
-    ],
+        plugins: [
+            'karma-mocha',
+            'karma-chai',
+            'karma-browserify',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher'
+        ],
 
-    // frameworks to use
-    frameworks: ['browserify', 'mocha', 'chai'],
+        // frameworks to use
+        frameworks: ['browserify', 'mocha', 'chai'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'test/**/*.js'
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'test/**/*.js'
+        ],
 
+        // list of files to exclude
+        exclude: [
 
-    // list of files to exclude
-    exclude: [
+        ],
 
-    ],
+        // Browserify config (all optional)
+        browserify: {
+            // extensions: ['.coffee'],
+            // ignore: [],
+            // transform: ['coffeeify'],
+            // debug: true,
+            // noParse: ['jquery'],
+            // watch: true
+        },
 
-    // Browserify config (all optional)
-    browserify: {
-      // extensions: ['.coffee'],
-      // ignore: [],
-      // transform: ['coffeeify'],
-      // debug: true,
-      // noParse: ['jquery'],
-      // watch: true
-    },
+        // Add browserify to preprocessors
+        preprocessors: {
+            'test/**/*.js': ['browserify']
+        },
 
-    // Add browserify to preprocessors
-    preprocessors: {'test/**/*.js': ['browserify']},
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress'],
 
+        // web server port
+        port: 9876,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_DEBUG,
 
-    // web server port
-    port: 9876,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera (has to be installed with `npm install karma-opera-launcher`)
+        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+        // - PhantomJS
+        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+        browsers: [
+            'Chrome',
+            'Firefox'
+        ],
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
 
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: [
-        'Chrome'//,
-    //    'Firefox'
-    ],
-
-
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
-
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
 };
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,12 +1,10 @@
 'use strict';
 
 describe('A test suite', function() {
-	beforeEach(function() {
-	});
-	afterEach(function() {
-	});
+    beforeEach(function() {});
+    afterEach(function() {});
 
-	it('should pass', function() {
-		expect(true).to.be.true;
-	});
+    it('should pass', function() {
+        expect(true).to.be.true;
+    });
 });
