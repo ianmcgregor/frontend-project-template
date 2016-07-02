3c8c669ef7d8d799b43333ae70bd52a7d9658800
Ian McGregor <ian@mcgregor.codes>
2016-07-02 18:31:48 +0100

    Add core js and usfl polyfills

---
+++ src/utils/polyfills/index.js
@@ -0,0 +1,18 @@
+/*
+* core js: https://github.com/zloirock/core-js
+*/
+
+import 'core-js/fn/array/from';
+import 'core-js/fn/array/includes';
+import 'core-js/fn/object/assign';
+import 'core-js/fn/promise';
+import 'core-js/fn/math/sign';
+import 'core-js/fn/string/includes';
+
+/*
+* usfl: https://github.com/ianmcgregor/usfl/blob/master/docs/README.md#polyfill
+*/
+
+import 'usfl/polyfill/classList';
+// import 'usfl/polyfill/console';
+import 'usfl/polyfill/requestAnimationFrame';
